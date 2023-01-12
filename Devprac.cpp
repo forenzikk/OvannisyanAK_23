a4623c5c816d69dc4d7a9c67ae797fc0943168a1
forenzikkk <sddivanov@yandex.ru>
2023-01-12 11:03:32 +0300

    On branch main  Initial commit 	new file:   Devprac.cpp

---
+++ Devprac.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <windows.h>
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int a, b, c;
+	printf("Введите первое число: ");
+	scanf_s("%d", &a);
+	printf("\nВведите второе число: ");
+	scanf_s("%d", &b);
+	c = a + b;
+	printf("\nРезультат = %d", c);
+}
